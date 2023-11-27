209fe7ad0d8bce989245fcea922f83b83e4b9ea3
Laura López <lauralopez166@gmail.com>
2023-11-27 08:51:11 +0100

    Elaboramos la clase tarea

---
+++ scripts/tarea.js
@@ -0,0 +1,10 @@
+ class Tarea {
+    constructor(id, descripcion){
+        this.id = id;
+        this.descripcion = descripcion;
+    }
+
+    editar(descripcion) { 
+        this.descripcion = descripcion;
+    }
+ }
